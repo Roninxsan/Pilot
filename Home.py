b7257c585cf76fc8515b3974d82683661545314b
Roninxsan <162503958+Roninxsan@users.noreply.github.com>
2024-03-16 18:02:58 +0000

    tutorial

--- Home.py
+++ Home.py
@@ -1,1 +1,7 @@
 import streamlit as st
+from config import page_setup as ps
+
+st.header("Hey Jake, thanks for the tutorial!" )
+("---")
+
+ps.title_pages()
---
+++ config/page_setup.py
@@ -0,0 +1,4 @@
+import streamlit as st
+
+def title_pages():
+    st.title("I like this title")
